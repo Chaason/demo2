5eb04bb38514ad45cd3449091f34a8d0f6f4ac78
NobuteruKumai <171550188+Chaason@users.noreply.github.com>
2024-07-10 12:16:52 +0900

    認証機能の実装完了

--- src/main/java/com/example/demo2/GeneratePassword.java
+++ src/main/java/com/example/demo2/GeneratePassword.java
@@ -5,18 +5,18 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 public class GeneratePassword {
 
 	public static void main(String[] args) {
-		
+		// ハッシュ化したいパスワードを入力
 		String rawPassword = "admin";
-		
-		String password = getEncodePassword(rawPassword);
-		
-		System.out.println(password);
 
+		// パスワードをハッシュ化
+		String password = getEncodePassword(rawPassword);
+
+		// ハッシュ化された値を表示
+		System.out.println(password);
 	}
-	
+
 	private static String getEncodePassword(String rawPassword) {
 		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 		return passwordEncoder.encode(rawPassword);
 	}
-
 }
